eb9f6d4fef8463a60ee2b48b95d2916465a56997
Nikita <nikita.gubarev31@gmail.com>
2023-08-30 16:51:20 +0300

    Add I18N_DOMAIN, BASE_DIR and LOCALES_DIR into the data/config.py

--- data/config.py
+++ data/config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -10,3 +11,7 @@ ADMINS = [
     int(admin_chat_id) for admin_chat_id in str(os.getenv("ADMINS")).split(",")
 ]
 SQLALCHEMY_DATABASE_URL = str(os.getenv("DATABASE_URL"))
+
+I18N_DOMAIN = "easypostbot"
+BASE_DIR = Path(__file__).parent.parent
+LOCALES_DIR = BASE_DIR / "locales"
