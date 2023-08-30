729b5ae0b96e64025e0ddc10d4c044593e46fffc
Nikita <nikita.gubarev31@gmail.com>
2023-08-30 15:01:33 +0300

    Add creating user into the start command handler

--- handlers/users/commands/start.py
+++ handlers/users/commands/start.py
@@ -2,9 +2,18 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from utils.db.models import User
+from utils.db.db import add_commit_and_refresh
 
 
 @dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
-    """Handles /start command to greet user."""
+    """Handles /start command to create and greet user."""
+    add_commit_and_refresh(
+        User(
+            chat_id=message.from_user.id,
+            username=message.from_user.username,
+            full_name=message.from_user.full_name,
+        )
+    )
     await message.answer(f"Hello, {message.from_user.full_name}!")
