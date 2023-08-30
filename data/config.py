39c327d06449574701a28de1972a716d1117cd68
Nikita <nikita.gubarev31@gmail.com>
2023-08-30 14:31:07 +0300

    Add SQLALCHEMY_DATABASE_URL into the data/config.py

--- data/config.py
+++ data/config.py
@@ -9,3 +9,4 @@ BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 ADMINS = [
     int(admin_chat_id) for admin_chat_id in str(os.getenv("ADMINS")).split(",")
 ]
+SQLALCHEMY_DATABASE_URL = str(os.getenv("DATABASE_URL"))
