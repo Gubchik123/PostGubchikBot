6873e9762006cd528599912af15ad18cc27886e2
Nikita <nikita.gubarev31@gmail.com>
2023-08-31 09:50:20 +0300

    Create keyboards/inline/language.py with language choice kb

---
+++ keyboards/inline/language.py
@@ -0,0 +1,17 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def get_language_inline_keyboard() -> InlineKeyboardMarkup:
+    """Returns InlineKeyboardMarkup with language buttons."""
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Русский", callback_data="set_language_ru"
+                ),
+                InlineKeyboardButton(
+                    text="English", callback_data="set_language_en"
+                ),
+            ],
+        ],
+    )
