7996a96fd22dea0df71403e77463e8c053f1cec6
Nikita <nikita.gubarev31@gmail.com>
2023-08-31 13:42:50 +0300

    Add calling menu after gretting user
    into the 'greet_user' function of the handlers/users/callback_handlers.py

--- handlers/users/callback_handlers.py
+++ handlers/users/callback_handlers.py
@@ -1,10 +1,12 @@
 from aiogram import types
 
 from loader import _
+from .menu import show_menu
 
 
 async def greet_user(query: types.CallbackQuery):
     """Greets user after from /language command."""
-    await query.message.edit_text(
+    await query.answer(
         _("Hello, {name}!").format(name=query.from_user.full_name)
     )
+    await show_menu(query)
