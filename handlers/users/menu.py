ed058b83c696493ae280de2bbbde5b40fefe1a55
Nikita <nikita.gubarev31@gmail.com>
2023-09-01 16:02:18 +0300

    Remake menu handler to be ready to get both Message & CallbackQuery

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -1,27 +1,41 @@
-from aiogram import types
+from typing import Optional, Callable
+
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message, CallbackQuery
 
 from loader import dp, _
 from keyboards.inline.menu import get_menu_keyboard
 from utils.db.user_crud import get_user_channels_by_
 
 
-@dp.callback_query_handler(text="menu")
-async def show_menu(query: types.CallbackQuery) -> None:
-    """Shows menu."""
-    user_channels = get_user_channels_by_(query.from_user.id)
+@dp.callback_query_handler(text="menu", state="*")
+async def show_menu(
+    data: Message | CallbackQuery, state: Optional[FSMContext] = None
+) -> None:
+    """
+    Shows order sends menu depending on the given data (message or callback).
+    """
+    if state:
+        await state.finish()
+    user_channels = get_user_channels_by_(data.from_user.id)
     has_channels = bool(user_channels)
+    answer_function: Callable = (
+        data.message.edit_text
+        if isinstance(data, CallbackQuery)
+        else data.answer
+    )
     if has_channels:
         channel_list_items = "\n".join(
-            [f"<li>{channel.title}</li>" for channel in user_channels]
+            [f"* {channel.title}\n" for channel in user_channels]
         )
-        await query.message.edit_text(
-            text=_(
-                "Your channels:\n" "<ul>{channel_list_items}</ul>\n"
-            ).format(channel_list_items=channel_list_items),
+        await answer_function(
+            text=_("Your channels:\n" "{channel_list_items}\n").format(
+                channel_list_items=channel_list_items
+            ),
             reply_markup=get_menu_keyboard(has_channels=True),
         )
     else:
-        await query.message.edit_text(
+        await answer_function(
             text=_(
                 "You currently have no channels.\n\n"
                 "Use the button below or the /add command to add it."
