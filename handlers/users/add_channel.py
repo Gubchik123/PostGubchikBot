1249145cc68c388b8a3b4e618b3dee682577fa00
Nikita <nikita.gubarev31@gmail.com>
2023-09-02 13:52:27 +0300

    Add function for checking if user can add one more channel
    into the "add_channel" callback query handler

--- handlers/users/add_channel.py
+++ handlers/users/add_channel.py
@@ -1,24 +1,46 @@
-from typing import Callable
-
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
 
 from loader import dp, _
 from states.channel import Channel
 from .commands.menu import show_menu
-from keyboards.inline.menu import get_back_to_menu_inline_button
 from utils.db.channel_crud import create_channel_by_
+from keyboards.inline.menu import get_back_to_menu_inline_button
+from utils.db.user_crud import get_user_by_, get_user_channels_by_
 
 
 @dp.callback_query_handler(text="add_channel")
-async def wait_for_forwarded_message(data: Message | CallbackQuery) -> None:
-    """Asks for forwarded message to the channel and waits for it."""
-    answer_function: Callable = (
-        data.message.edit_text
-        if isinstance(data, CallbackQuery)
-        else data.answer
-    )
-    await answer_function(
+async def check_if_user_can_add_channel(query: CallbackQuery) -> None:
+    """Checks if user can add channel
+    and shows the next instructions if True and message otherwise."""
+    if not _can_user_add_channel(query.from_user.id):
+        await query.message.edit_text(
+            _("You have reached the limit of channels!"),
+            reply_markup=InlineKeyboardMarkup().add(
+                get_back_to_menu_inline_button()
+            ),
+        )
+        return
+    await ask_for_forwarded_message(query)
+
+
+def _can_user_add_channel(user_chat_id: int) -> bool:
+    """Returns True if user can add channel and False otherwise."""
+    user = get_user_by_(user_chat_id)
+    user_channels_count = len(get_user_channels_by_(user_chat_id))
+    if not user.subscription_id and user_channels_count + 1 > 3:
+        return False
+    elif (
+        user.subscription_id
+        and user_channels_count + 1 > user.subscription.max_channels
+    ):
+        return False
+    return True
+
+
+async def ask_for_forwarded_message(query: CallbackQuery) -> None:
+    """Asks for forwarded message to the channel and waits (state) for it."""
+    await query.message.edit_text(
         _(
             "Forward a message here from the channel you want to add.\n\n"
             "<b>Important:</b> the bot and you must be administrators"
