3f726c4b7e6492c522a618e94090c64964d3c901
Nikita <nikita.gubarev31@gmail.com>
2023-09-02 13:54:07 +0300

    Add new crud subscription function for adding subscription for user

--- utils/db/subscription_crud.py
+++ utils/db/subscription_crud.py
@@ -1,8 +1,24 @@
-from .db import MySession
 from .models import Subscription
+from .user_crud import _get_user_by_
+from .db import MySession, commit_and_refresh
 
 
 def get_all_subscriptions() -> list[Subscription]:
     """Returns all subscriptions ordered by id."""
     with MySession() as session:
         return session.query(Subscription).all()
+
+
+def add_subscription_for_user_with_(
+    chat_id: int, subscription_price: int
+) -> None:
+    """Adds subscription for user with the given chat id."""
+    with MySession() as session:
+        user = _get_user_by_(session, chat_id)
+        user.subscription_id = (
+            session.query(Subscription)
+            .filter(Subscription.price == subscription_price)
+            .first()
+            .id
+        )
+        commit_and_refresh(session, user)
