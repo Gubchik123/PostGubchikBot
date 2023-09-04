901afd182c4be5eb8119635da45012fb46f9612e
Nikita <nikita.gubarev31@gmail.com>
2023-09-04 18:20:40 +0300

    Add message arfs into the crud function for creating user
    in the start command

--- handlers/users/commands/start.py
+++ handlers/users/commands/start.py
@@ -10,5 +10,5 @@ from .language import language_command
 async def start_command(message: types.Message):
     """Handles /start command to create and greet user."""
     if get_user_by_(message.from_user.id) is None:
-        create_user_by_(message.from_user)
+        create_user_by_(message.from_user, message.get_args())
     await language_command(message)
