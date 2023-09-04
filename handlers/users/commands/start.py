f7e27c06cacaab53883ab610415efb63e6826f4a
Nikita <nikita.gubarev31@gmail.com>
2023-09-04 13:50:12 +0300

    Add checking if user exists before creating into the start command

--- handlers/users/commands/start.py
+++ handlers/users/commands/start.py
@@ -2,12 +2,13 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp, _
-from utils.db.user_crud import create_user_by_
+from utils.db.user_crud import get_user_by_, create_user_by_
 from .language import language_command
 
 
 @dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
     """Handles /start command to create and greet user."""
-    create_user_by_(message.from_user)
+    if get_user_by_(message.from_user.id) is None:
+        create_user_by_(message.from_user)
     await language_command(message)
