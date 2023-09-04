13f7b881fa96eee226919b187d3c3b53c45c90eb
Nikita <nikita.gubarev31@gmail.com>
2023-09-04 13:51:06 +0300

    Remove catching the IntegrityError from the 'add_commit_and_refresh'

--- utils/db/db.py
+++ utils/db/db.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +8,7 @@ from data.config import SQLALCHEMY_DATABASE_URL
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-MySession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+MySession = sessionmaker(engine)
 
 Base = declarative_base()
 
@@ -23,9 +22,6 @@ def commit_and_refresh(session: Session, model: Base):
 
 def add_commit_and_refresh(model: Base):
     """Adds, commits and refreshes model instance and returns it."""
-    session = MySession()
-    try:
+    with MySession() as session:
         session.add(model)
         return commit_and_refresh(session, model)
-    except IntegrityError:
-        pass
