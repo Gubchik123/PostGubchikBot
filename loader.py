6de1f9e021281bd7a40c1d71abddb09f6d8c80e7
Nikita <nikita.gubarev31@gmail.com>
2023-09-04 16:12:16 +0300

    Add scheduler (AsyncIOScheduler) into the loader.py

--- loader.py
+++ loader.py
@@ -1,14 +1,40 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
-from data.config import BOT_TOKEN, I18N_DOMAIN, LOCALES_DIR
-from middlewares import ACLMiddleware, ThrottlingMiddleware
+from utils.db.db import engine, Base
+from data.config import (
+    BOT_TOKEN,
+    I18N_DOMAIN,
+    LOCALES_DIR,
+    DEFAULT_TIMEZONE,
+    SCHEDULER_JOBS_DATABASE_URL,
+)
+from middlewares import (
+    ACLMiddleware,
+    ThrottlingMiddleware,
+)
 
 
 storage = MemoryStorage()
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
+
+scheduler = AsyncIOScheduler(
+    jobstores={
+        "default": SQLAlchemyJobStore(
+            url=SCHEDULER_JOBS_DATABASE_URL,
+            engine=engine,
+            tablename="apscheduler_jobs",
+            metadata=Base.metadata,
+        )
+    },
+    timezone=DEFAULT_TIMEZONE,
+)
+scheduler.start()
+scheduler.print_jobs()
 
 i18n = ACLMiddleware(I18N_DOMAIN, LOCALES_DIR)
 _ = i18n.gettext  # Alias for translation
