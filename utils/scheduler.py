7d78949a1b62d4468b95a91162456beac84e7358
Nikita <nikita.gubarev31@gmail.com>
2023-09-04 16:16:52 +0300

    Add scheduler job function for removing user subscription
    into the utils/scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,4 +1,6 @@
 from loader import bot
+from utils.db.user_crud import _get_user_by_
+from utils.db.db import MySession, commit_and_refresh
 
 
 async def send_subscription_reminder_to_user(
@@ -25,5 +27,23 @@ async def send_subscription_reminder_to_user(
             f"Твоя подписка закончится через {period_left} {period_plural_name}!"
             if user_language_code == "ru"
             else f"Your subscription will expire in {period_left} {period_plural_name}!"
+        ),
+    )
+
+
+async def remove_user_subscription_by_(user_chat_id: int) -> None:
+    """Removes user subscription by the given user chat id."""
+    with MySession() as session:
+        user = _get_user_by_(session, user_chat_id)
+        if user and user.subscription:
+            user.subscription = None
+            user.subscription_expire_date = None
+            commit_and_refresh(session, user)
+    await bot.send_message(
+        user_chat_id,
+        text=(
+            "Твоя подписка закончилась!"
+            if user.language_code == "ru"
+            else "Your subscription has expired!"
         ),
     )
