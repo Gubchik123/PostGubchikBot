ed9380a2c588a1b0a988dd071c2be641a571c95a
Nikita <nikita.gubarev31@gmail.com>
2023-09-06 15:13:28 +0300

    Add message_ids into the PostContent
    in the utils/post_content.py

--- utils/post_content.py
+++ utils/post_content.py
@@ -13,12 +13,18 @@ class PostContent:
     def clear(self) -> None:
         """Clears the post content."""
         self.content = []
+        self.clear_message_ids()
+
+    def clear_message_ids(self) -> None:
+        """Clears the message ids."""
+        self.message_ids = []
 
     async def send_to_(self, chat_id: int) -> None:
         """Sends the post content to the given chat."""
         for content in self.content:
             send_function: Callable = getattr(bot, f"send_{content['type']}")
-            await send_function(chat_id, content["content"])
+            message = await send_function(chat_id, content["content"])
+            self.message_ids.append(message.message_id)
 
     def add(self, type: str, content: str) -> None:
         """Adds the content to the post content."""
