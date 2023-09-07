9de207abc1118b863daefee5693e8094cc4160cf
Nikita <nikita.gubarev31@gmail.com>
2023-09-07 10:47:39 +0300

    Create handlers/users/post/publishing.py

---
+++ handlers/users/post/publishing.py
@@ -0,0 +1,45 @@
+from typing import Callable
+
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message, CallbackQuery
+
+from loader import dp, _
+from utils.db.user_crud import get_user_channels_by_
+from keyboards.inline.post import get_pre_publish_keyboard
+
+from ..commands.menu import show_menu
+from .constants import post_content, selected_channels
+
+
+@dp.callback_query_handler(text="time_to_publish_post", state="*")
+async def ask_about_time_to_publish_post(
+    data: Message | CallbackQuery, state: FSMContext
+) -> None:
+    """Asks about time to publish the post."""
+    if state:
+        await state.finish()
+    answer_function: Callable = (
+        data.message.edit_text
+        if isinstance(data, CallbackQuery)
+        else data.answer
+    )
+    await answer_function(
+        text=_(
+            "Post content is ready to publish into: {selected_channels}\n\n"
+            "Do you want to publish it now or later?"
+        ).format(selected_channels=", ".join(selected_channels)),
+        reply_markup=get_pre_publish_keyboard(),
+    )
+
+
+async def publish_post(query: CallbackQuery, *args) -> None:
+    """Publishes the post into the selected channels."""
+    await query.message.answer(_("Publishing..."))
+    for channel in get_user_channels_by_(query.from_user.id):
+        if channel.title in selected_channels:
+            await post_content.send_to_(channel.chat_id)
+    await query.message.answer(_("Published!"))
+    post_content.clear()
+    # ! Workaround with user chat id to avoid "No channels" in the menu handler
+    query.message.from_user.id = query.from_user.id
+    await show_menu(query.message)
