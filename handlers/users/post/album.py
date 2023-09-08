9aa5159bbffe23201ac1c37e229ce5a63ccf8989
Nikita <nikita.gubarev31@gmail.com>
2023-09-08 15:33:07 +0300

    Add checking on album media_type before adding new item
    in the handlers of the handlers/users/post/album.py

--- handlers/users/post/album.py
+++ handlers/users/post/album.py
@@ -30,28 +30,47 @@ async def add_text_to_post(message: types.Message, state: FSMContext) -> None:
 )
 async def handle_animation(message: types.Message):
     """Adds animation to the post album."""
-    post_album.add(types.InputMediaAnimation(media=message.animation.file_id))
+    if not post_album.media_type or post_album.media_type == "animation":
+        post_album.add(
+            types.InputMediaAnimation(media=message.animation.file_id)
+        )
+    else:
+        await message.answer(_("You can't add animation into this album!"))
 
 
 @dp.message_handler(content_types=types.ContentType.DOCUMENT, state=Post.album)
 async def handle_document(message: types.Message):
     """Adds document to the post album."""
-    post_album.add(types.InputMediaDocument(media=message.document.file_id))
+    if not post_album.media_type or post_album.media_type == "document":
+        post_album.add(
+            types.InputMediaDocument(media=message.document.file_id)
+        )
+    else:
+        await message.answer(_("You can't add document into this album!"))
 
 
 @dp.message_handler(content_types=types.ContentType.AUDIO, state=Post.album)
 async def handle_audio(message: types.Message):
     """Adds audio to the post album."""
-    post_album.add(types.InputMediaAudio(media=message.audio.file_id))
+    if not post_album.media_type or post_album.media_type == "audio":
+        post_album.add(types.InputMediaAudio(media=message.audio.file_id))
+    else:
+        await message.answer(_("You can't add audio into this album!"))
 
 
 @dp.message_handler(content_types=types.ContentType.PHOTO, state=Post.album)
 async def handle_photo(message: types.Message):
-    """Adds ] to the post album."""
-    post_album.add(types.InputMediaPhoto(media=message.photo[-1].file_id))
+    """Adds photo to the post album."""
+    if not post_album.media_type or post_album.media_type == "photo":
+        post_album.add(types.InputMediaPhoto(media=message.photo[-1].file_id))
+    else:
+        await message.answer(_("You can't add photo into this album!"))
 
 
 @dp.message_handler(content_types=types.ContentType.VIDEO, state=Post.album)
 async def handle_video(message: types.Message):
     """Adds video to the post album."""
-    post_album.add(types.InputMediaVideo(media=message.video.file_id))
+    if not post_album.media_type or post_album.media_type == "video":
+        post_album.add(types.InputMediaVideo(media=message.video.file_id))
+    else:
+        await message.answer(_("You can't add video into this album!"))
