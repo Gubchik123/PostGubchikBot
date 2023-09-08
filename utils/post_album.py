7aa154c3c80945db428c2826d4d6e0cba3ccb318
Nikita <nikita.gubarev31@gmail.com>
2023-09-08 13:01:44 +0300

    Create utils/post_album.py

---
+++ utils/post_album.py
@@ -0,0 +1,21 @@
+from aiogram.types import MediaGroup, InputMedia
+
+
+class PostAlbum:
+    """Class for storing post album."""
+
+    def __init__(self) -> None:
+        """Initializes the post empty album."""
+        self.clear()
+
+    def clear(self) -> None:
+        """Clears the post album."""
+        self.album = []
+
+    def get_album(self) -> MediaGroup:
+        """Returns the album (MediaGroup)."""
+        return MediaGroup(self.album)
+    
+    def add(self, input_media: InputMedia) -> None:
+        """Adds the input media to the post album."""
+        self.album.append(input_media)
