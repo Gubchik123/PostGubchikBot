b3d7a3aad951c04dc5651c1be446f640600157e0
Nikita <nikita.gubarev31@gmail.com>
2023-09-09 16:09:56 +0300

    Update bot commands for en and ru languages
    in the utils/bot_commands.py

--- utils/bot_commands.py
+++ utils/bot_commands.py
@@ -1,11 +1,28 @@
-from aiogram import types
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+from loader import bot
 
 
 async def set_default_bot_commands(dp) -> None:
-    """Sets default bot commands."""
-    await dp.bot.set_my_commands(
-        [
-            types.BotCommand("start", "Start the bot"),
-            types.BotCommand("help", "Get help"),
-        ]
-    )
+    """Sets default bot commands for en and ru languages."""
+    bot_commands = {
+        "en": [
+            BotCommand("start", "Start working with the bot"),
+            BotCommand("help", "Send of basic usage rules"),
+            BotCommand("language", "Change the language of the bot"),
+            BotCommand("menu", "Send main menu"),
+        ],
+        "ru": [
+            BotCommand("start", "Начало работы з ботом"),
+            BotCommand("help", "Отображение основных правил использования"),
+            BotCommand("language", "Изменения языка бота"),
+            BotCommand("menu", "Отправить главное меню"),
+        ],
+    }
+
+    for language_code, commands in bot_commands.items():
+        await bot.set_my_commands(
+            commands=commands,
+            scope=BotCommandScopeDefault(),
+            language_code=language_code,
+        )
