8c7908671e75cb3d42db63f2617bf86076e7682e
Nikita <nikita.gubarev31@gmail.com>
2023-09-11 18:27:29 +0300

    Add reliazation for handlers that do smth with posts in queue
    for all instead of 'for adding watermark'
    in the handlers/users/post/in_queue.py

--- bot/handlers/users/post/in_queue.py
+++ bot/handlers/users/post/in_queue.py
@@ -1,4 +1,5 @@
 from pytz import timezone
+from random import shuffle
 from datetime import datetime, timedelta
 from typing import Union, Optional, List, Tuple, Callable
 
@@ -7,9 +8,18 @@ from aiogram.types import Message, CallbackQuery
 
 from loader import dp, scheduler, _
 from utils.db.crud.user import get_user_by_
+from utils.post.settings import PostSettings
 from states.post_in_queue import PostInQueue
+from states.post_settings import PostContentSettings
 from utils.scheduler import publish_user_post_in_queue
-from keyboards.inline.callback_data import posts_in_queue_callback_data
+from messages.post import get_asking_for_url_buttons_text
+from keyboards.inline.post.content import (
+    get_url_buttons_from_,
+)  # TODO: Rename it to the 'get_url_buttons_keyboard_from_'
+from keyboards.inline.callback_data import (
+    posts_in_queue_callback_data,
+    get_keyboard_with_back_inline_button_by_,
+)
 from keyboards.inline.post.in_queue import (
     get_posts_in_queue_keyboard,
     get_date_keyboard,
@@ -26,6 +36,7 @@ from .postponing import generate_random_id
 @dp.callback_query_handler(text="posts_in_queue")
 async def get_channels_(callback_query: CallbackQuery) -> None:
     """Sends a message with inline keyboard to select a channel."""
+    constants.post_content.settings = PostSettings()
     await get_channels(callback_query, target="post in queue")
 
 
@@ -40,22 +51,52 @@ async def ask_for_what_to_add_to_posts_in_queue(
     )
     await answer_function(
         _("Select what you want to add to all posts in the queue:"),
-        reply_markup=get_posts_in_queue_keyboard(),
+        reply_markup=get_posts_in_queue_keyboard(
+            constants.post_content.settings
+        ),
     )
 
 
-async def add_signature_to_posts_in_queue(
+async def ask_for_caption_to_posts_in_queue(
     callback_query: CallbackQuery,
 ) -> None:
-    """Adds signature to posts in queue."""
-    await callback_query.answer(_("Soon..."))
+    """Asks for caption to posts in queue and waits (state) for it."""
+    await callback_query.message.edit_text(
+        text=_("Send me new caption for all posts in the queue."),
+        reply_markup=get_keyboard_with_back_inline_button_by_(
+            callback_data="posts_in_queue:ask_for_what_to_add:None"
+        ),
+    )
+    await PostContentSettings.caption.set()
 
 
-async def add_url_buttons_to_posts_in_queue(
+@dp.message_handler(state=PostContentSettings.caption)
+async def add_caption_to_posts_in_queue(message: Message) -> None:
+    """Adds caption to posts in queue."""
+    constants.post_content.settings.caption = message.text
+    await ask_for_what_to_add_to_posts_in_queue(message)
+
+
+async def ask_for_url_buttons_to_posts_in_queue(
     callback_query: CallbackQuery,
 ) -> None:
     """Adds url buttons to posts in queue."""
-    await callback_query.answer(_("Soon..."))
+    await callback_query.message.edit_text(
+        text=get_asking_for_url_buttons_text(),
+        reply_markup=get_keyboard_with_back_inline_button_by_(
+            callback_data="posts_in_queue:ask_for_what_to_add:None"
+        ),
+    )
+    await PostContentSettings.reply_markup.set()
+
+
+@dp.message_handler(state=PostContentSettings.reply_markup)
+async def add_url_buttons_to_posts_in_queue(message: Message) -> None:
+    """Adds url buttons to posts in queue."""
+    constants.post_content.settings.reply_markup = get_url_buttons_from_(
+        message.text
+    )
+    await ask_for_what_to_add_to_posts_in_queue(message)
 
 
 async def add_watermark_to_posts_in_queue(
@@ -65,35 +106,46 @@ async def add_watermark_to_posts_in_queue(
     await callback_query.answer(_("Soon..."))
 
 
-async def remove_author_text_from_posts_in_queue(
-    callback_query: CallbackQuery,
-) -> None:
-    """Removes author text from posts in queue."""
-    await callback_query.answer(_("Soon..."))
-
-
 async def disable_web_page_preview_for_posts_in_queue(
     callback_query: CallbackQuery,
 ) -> None:
     """Disables web page preview for posts in queue."""
-    await callback_query.answer(_("Soon..."))
+    constants.post_content.settings.disable_web_page_preview = (
+        not constants.post_content.settings.disable_web_page_preview
+    )
+    await callback_query.message.edit_reply_markup(
+        reply_markup=get_posts_in_queue_keyboard(
+            constants.post_content.settings
+        )
+    )
 
 
 async def disable_notification_for_posts_in_queue(
     callback_query: CallbackQuery,
 ) -> None:
     """Disables notification for posts in queue."""
-    await callback_query.answer(_("Soon..."))
-
-
-async def send_posts_in_queue_as_album(callback_query: CallbackQuery) -> None:
-    """Sends posts in queue as album."""
-    await callback_query.answer(_("Soon..."))
+    constants.post_content.settings.disable_notification = (
+        not constants.post_content.settings.disable_notification
+    )
+    await callback_query.message.edit_reply_markup(
+        reply_markup=get_posts_in_queue_keyboard(
+            constants.post_content.settings
+        )
+    )
 
 
 async def shuffle_posts_in_queue(callback_query: CallbackQuery) -> None:
     """Shuffles posts in queue."""
-    await callback_query.answer(_("Soon..."))
+    if not constants.post_content.settings.shuffle:
+        shuffle(constants.post_content.content)
+        constants.post_content.settings.shuffle = True
+        await callback_query.message.edit_reply_markup(
+            reply_markup=get_posts_in_queue_keyboard(
+                constants.post_content.settings
+            )
+        )
+    else:
+        await callback_query.answer(_("Posts are already shuffled."))
 
 
 global_start_date = ""
@@ -318,6 +370,11 @@ def _schedule_job_to_publish_user_post_in_queue(
     run_date: datetime,
 ) -> None:
     """Schedules job to publish user post in queue on the given date."""
+    post_content_item[
+        "kwargs"
+    ] = constants.post_content.settings.get_kwargs_by_(
+        post_content_item["type"]
+    )
     scheduler.add_job(
         publish_user_post_in_queue,
         "date",
@@ -358,13 +415,11 @@ async def navigate_all_other_posts_in_queue_callback_data(
     current_level_function: Callable = {
         "ask_for_what_to_add": ask_for_what_to_add_to_posts_in_queue,
         # handlers to do something with posts in queue
-        "add_signature": add_signature_to_posts_in_queue,
-        "add_url_buttons": add_url_buttons_to_posts_in_queue,
+        "add_caption": ask_for_caption_to_posts_in_queue,
+        "add_url_buttons": ask_for_url_buttons_to_posts_in_queue,
         "add_watermark": add_watermark_to_posts_in_queue,
-        "remove_author_text": remove_author_text_from_posts_in_queue,
         "disable_web_page_preview": disable_web_page_preview_for_posts_in_queue,
         "disable_notification": disable_notification_for_posts_in_queue,
-        "send_posts_in_queue_as_album": send_posts_in_queue_as_album,
         "shuffle": shuffle_posts_in_queue,
         # handlers to set up posts in queue sending
         "ask_for_start_date": ask_for_start_date_to_send_posts_in_queue,
