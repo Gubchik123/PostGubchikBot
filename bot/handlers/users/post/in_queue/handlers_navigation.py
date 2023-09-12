261c4dec3b17919a020478ff550e644ef6b828cc
Nikita <nikita.gubarev31@gmail.com>
2023-09-12 18:01:40 +0300

    Create handlers/users/post/in_queue/settings/ dir to divide files

--- bot/handlers/users/post/in_queue/handlers_navigation.py
+++ bot/handlers/users/post/in_queue/handlers_navigation.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher import FSMContext
 from loader import dp
 from keyboards.inline.callback_data import posts_in_queue_callback_data
 
-from . import settings, time
+from . import time, settings
 
 
 @dp.callback_query_handler(posts_in_queue_callback_data.filter(), state="*")
@@ -19,14 +19,14 @@ async def navigate_all_other_posts_in_queue_callback_data(
     if state:
         await state.finish()
     current_level_function: Callable = {
-        "ask_for_what_to_add": settings.ask_for_what_to_add_to_posts_in_queue,
+        "ask_for_what_to_add": settings.ask.ask_for_what_to_add_to_posts_in_queue,
         # handlers to do something with posts in queue
-        "add_caption": settings.ask_for_caption_to_posts_in_queue,
-        "add_url_buttons": settings.ask_for_url_buttons_to_posts_in_queue,
-        "add_watermark": settings.ask_for_watermark_to_posts_in_queue,
-        "disable_web_page_preview": settings.disable_web_page_preview_for_posts_in_queue,
-        "disable_notification": settings.disable_notification_for_posts_in_queue,
-        "shuffle": settings.shuffle_posts_in_queue,
+        "add_caption": settings.caption.ask_for_caption_to_posts_in_queue,
+        "add_url_buttons": settings.url_buttons.ask_for_url_buttons_to_posts_in_queue,
+        "add_watermark": settings.watermark.ask_for_watermark_to_posts_in_queue,
+        "disable_web_page_preview": settings.other.disable_web_page_preview_for_posts_in_queue,
+        "disable_notification": settings.other.disable_notification_for_posts_in_queue,
+        "shuffle": settings.other.shuffle_posts_in_queue,
         # handlers to set up posts in queue sending
         "ask_for_start_date": time.ask_for_start_date_to_send_posts_in_queue,
         "ask_for_time": time.ask_for_time_to_send_posts_in_queue,
---
+++ bot/handlers/users/post/in_queue/settings/__init__.py
@@ -0,0 +1,5 @@
+from . import ask
+from . import caption 
+from . import url_buttons 
+from . import watermark 
+from . import other
