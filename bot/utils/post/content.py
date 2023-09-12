70f52b685570acb29f71e6b0309ce0305c001fe7
Nikita <nikita.gubarev31@gmail.com>
2023-09-12 15:04:26 +0300

    Add logice with images into the PostContent
    in the utils/post/content.py

--- bot/utils/post/content.py
+++ bot/utils/post/content.py
@@ -1,4 +1,7 @@
-from typing import Any, Callable, Optional
+import os
+from typing import Any, Callable, Optional, List
+
+from aiogram.types import InputFile
 
 from loader import bot
 
@@ -30,6 +33,18 @@ class PostContent:
         post_content.content.append(post_content_item)
         return post_content
 
+    @property
+    def is_there_images(self) -> bool:
+        """Returns True if there are images in the post content."""
+        return any(content["type"] == "photo" for content in self.content)
+
+    @property
+    def images(self) -> List[dict]:
+        """Returns the images from the post content."""
+        return [
+            content for content in self.content if content["type"] == "photo"
+        ]
+
     def clear(self) -> None:
         """Clears the post content."""
         self.content = []
@@ -46,10 +61,22 @@ class PostContent:
                 await bot.send_media_group(chat_id, media=content["content"])
                 break
             send_function: Callable = getattr(bot, f"send_{content['type']}")
-            message = await send_function(
-                chat_id, content["content"], **content["kwargs"]
-            )
+            if content["type"] == "photo" and content["content"].startswith(
+                "Path:"
+            ):
+                image_path = content["content"][5:]
+                with open(image_path, "rb") as photo:
+                    message = await send_function(
+                        chat_id, InputFile(photo), **content["kwargs"]
+                    )
+                os.remove(image_path)
+            else:
+                message = await send_function(
+                    chat_id, content["content"], **content["kwargs"]
+                )
             self.message_ids.append(message.message_id)
+
+    # * CRUD methods ----------------------------------------------------------
 
     def add(self, type: str, content: str) -> dict:
         """Adds the content to the post content and returns its index."""
@@ -65,6 +92,10 @@ class PostContent:
     def get_item_by_(self, content_index: int) -> dict:
         """Returns content item by the given index."""
         return self.content[content_index]
+
+    def update_by_(self, content_item_index: int, content: Any) -> None:
+        """Updates content item by the given index."""
+        self.content[content_item_index]["content"] = content
 
     def update_kwargs(
         self, content_index: int, key: str, value: Optional[Any] = None
